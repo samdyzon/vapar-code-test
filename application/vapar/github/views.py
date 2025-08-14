e3bd20ee84b0d3d9b63f0e9d8d9e253f5b97b0ee
Sam Dyson <mail@samdyson.com>
2025-08-14 15:38:23 +1000

    fix: update logic for rate limited and distinct error for request missing a query string

--- application/vapar/github/views.py
+++ application/vapar/github/views.py
@@ -16,15 +16,24 @@ async def repos(request):
     Returns:
         HttpResponse: The Response
     """
-    query = request.query_params.get("query", "")
+    query = request.query_params.get("query", None)
+
+    if not query:
+        return Response(
+            {"error": "You must provide a query."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"https://api.github.com/search/repositories?q={query}",
-            headers={"accept": "application/vnd.github+json"},
+            headers={
+                "accept": "application/vnd.github+json",
+                "user-agent": "MyApp/1.0",
+            },
         )
 
-        if response.status_code == 422:
+        # Assume: If you get a 422 but have a query, you are probably rate-limited?
+        if response.status_code == 429:
             return Response(
                 {"error": "You have been rate limited by GitHub, try again later."},
                 status=status.HTTP_429_TOO_MANY_REQUESTS,
@@ -62,7 +71,10 @@ async def repo(request, owner, repo):
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"https://api.github.com/repos/{owner}/{repo}",
-            headers={"accept": "application/vnd.github+json"},
+            headers={
+                "accept": "application/vnd.github+json",
+                "user-agent": "MyApp/1.0",
+            },
         )
 
         if response.status_code == 404:
