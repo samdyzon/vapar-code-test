fb83bdfa6044c5b1b86a2b7192d57d75f4c217fa
Sam Dyson <mail@samdyson.com>
2025-08-14 11:21:45 +1000

    fix: clean up frontend code and get ready for app integration

--- libs/frontend/src/VaparApp.tsx
+++ libs/frontend/src/VaparApp.tsx
@@ -1,22 +1,11 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-lg">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -32,4 +21,6 @@ function App() {
   )
 }
 
-export default App
+export const AppWrapper = () => {
+  return <App />
+}
